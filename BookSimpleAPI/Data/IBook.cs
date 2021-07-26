3e260b6601978d9de9af58a8bf218849037f0882
Jesper Petersen <info@ordklar.dk>
2021-07-26 22:29:33 +0200

    Oprettelse

---
+++ BookSimpleAPI/Data/IBook.cs
@@ -0,0 +1,21 @@
+﻿using BookSimpleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookSimpleAPI.Data
+{
+    public interface IBook
+    {
+
+        List<Book> Books();
+        Book GetBookSingle(int id);
+        Book GetBookSignelByName(string name);
+        List<Book> GetBooksByOrderPrice(int orderPrice);
+        List<Book> GetBooksByOrderTitle(string title);
+        List<Book> GetBooksByOrderDescription(string description);
+        List<Book> GetBooksByDate(int Date);
+
+    }
+}
---
+++ BookSimpleAPI/Data/MockBook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookSimpleAPI.Data
+{
+    public class MockBook
+    {
+    }
+}
