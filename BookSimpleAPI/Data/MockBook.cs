a9053e587b18cbb121b02813028e7cb13e7028b9
Jesper Petersen <info@ordklar.dk>
2021-07-26 22:32:26 +0200

    tilføj interface

--- BookSimpleAPI/Data/MockBook.cs
+++ BookSimpleAPI/Data/MockBook.cs
@@ -1,11 +1,46 @@
-﻿using System;
+﻿using BookSimpleAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookSimpleAPI.Data
 {
-    public class MockBook
+    public class MockBook : IBook
     {
+        public List<Book> Books()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetBooksByDate(int Date, string tags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetBooksByOrderDescription(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetBooksByOrderPrice(int orderPrice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetBooksByOrderTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBookSignelByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBookSingle(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
