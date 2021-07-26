7d88e25a5be332ccc4677d1776b6e9e0db23167d
Jesper Petersen <info@ordklar.dk>
2021-07-26 23:33:56 +0200

    Update Book.cs

--- BookSimpleAPI/Models/Book.cs
+++ BookSimpleAPI/Models/Book.cs
@@ -9,9 +9,7 @@ namespace BookSimpleAPI.Models
     {
         public int BookId { get; set; }
         public string Name { get; set; }
-        public string Author { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
         public decimal Price { get; set; }
         public DateTime DateTime { get; set; }
     }
