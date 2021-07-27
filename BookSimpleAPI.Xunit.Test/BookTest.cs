4a68741b1c277f241f09bb146c2bb2dfd24b1e1e
Jesper Petersen <info@ordklar.dk>
2021-07-27 14:25:58 +0200

    Create BookTest.cs

---
+++ BookSimpleAPI.Xunit.Test/BookTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BookSimpleAPI.Xunit.Test
+{
+    public class BookTest
+    {
+        [Fact]
+        public void GetListWithBook()
+        {
+
+        }
+    }
+}
