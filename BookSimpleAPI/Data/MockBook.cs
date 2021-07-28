2fe3539e06577a614b1157803ffe7ce941f17077
Jesper Petersen <info@ordklar.dk>
2021-07-28 02:28:41 +0200

    Update MockBook.cs

--- BookSimpleAPI/Data/MockBook.cs
+++ BookSimpleAPI/Data/MockBook.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookSimpleAPI.Helper.Extensions;
 
 namespace BookSimpleAPI.Data
 {
@@ -12,12 +13,12 @@ namespace BookSimpleAPI.Data
         {
             var book = new List<Book>()
             {
-                new Book {BookId = 1,  DateTime = new DateTime(2021, 1, 1), Name="Book Simple API", Price = 450, Title = "Google API Test"},
-                new Book {BookId = 2,  DateTime = new DateTime(2021, 7, 25),Name="Datamatiker", Price = 450, Title = "Google API Test"},
-                new Book {BookId = 3,  DateTime = new DateTime(2021, 5, 25), Name="Book Simple API", Price = 450, Title = "Google API Test"},
+                new Book {BookId = 1,  DateTime = new DateTime(2021, 1, 1), Name="Book Simple API", Price = 251, Title = "Google API Test"},
+                new Book {BookId = 2,  DateTime = new DateTime(2021, 7, 25),Name="Datamatiker", Price = 99, Title = "Google API Test"},
+                new Book {BookId = 3,  DateTime = new DateTime(2021, 5, 25), Name="Book Simple API", Price = 863, Title = "Google API Test"},
                 new Book {BookId = 4,  DateTime = new DateTime(2021, 8, 25), Name="Software", Price = 450, Title = "Google API Test"},
-                new Book {BookId = 5,  DateTime = new DateTime(2021, 4, 25), Name="Book", Price = 450, Title = "Google API Test"},
-                new Book {BookId = 6,  DateTime = new DateTime(2021, 3, 25),Name="Testing", Price = 450, Title = "Google API Test"}
+                new Book {BookId = 5,  DateTime = new DateTime(2021, 4, 25), Name="Book", Price = 467, Title = "Google API Test"},
+                new Book {BookId = 6,  DateTime = new DateTime(2021, 3, 25),Name="Testing", Price = 32, Title = "Google API Test"}
             };
             return book;           
         }
@@ -30,7 +31,7 @@ namespace BookSimpleAPI.Data
 
         public List<Book> GetBooksByDate(int Date, string tags)
         {
-            throw new NotImplementedException();
+            return BookList().ByDateAndTags(Date, tags);
         }
 
         public List<Book> GetBooksByOrderPrice(int orderPrice)
@@ -45,7 +46,7 @@ namespace BookSimpleAPI.Data
 
         public Book GetBookSingle(int id)
         {
-            throw new NotImplementedException();
+            return BookList().FirstOrDefault(r => r.BookId == id);
         }
     }
 }
