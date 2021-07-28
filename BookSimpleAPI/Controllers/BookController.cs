e118885cf46de5e85325982a25cf55032c29cff1
Jesper Petersen <info@ordklar.dk>
2021-07-28 02:28:38 +0200

    Update BookController.cs

--- BookSimpleAPI/Controllers/BookController.cs
+++ BookSimpleAPI/Controllers/BookController.cs
@@ -1,14 +1,12 @@
 ﻿using BookSimpleAPI.Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace BookSimpleAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/book")]
     [ApiController]
     public class BookController : ControllerBase
     {
@@ -20,13 +18,41 @@ namespace BookSimpleAPI.Controllers
         }
 
 
-        [HttpGet]
+        [HttpGet("all")]
         public ActionResult<IEnumerable<MockBook>> GetAll()
         {
             try
             {
-                var commandItem = _book.GetBooks();
-                return Ok(commandItem);
+                var result = _book.GetBooks();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpGet("DateAndTags/{days}/{tags}")]
+        public ActionResult <MockBook> GetBooksByDate(int days, string tags)
+        {
+            try
+            {
+                var resultBook = _book.GetBooksByDate(days, tags);
+                return Ok(resultBook);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult <MockBook> GetSingleById(int id)
+        {
+            try
+            {
+                var result = _book.GetBookSingle(id);
+                return Ok(result);
             }
             catch (Exception e)
             {
