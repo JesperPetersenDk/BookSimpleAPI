571090726b0390b72381efdc028cbaf1df3f9567
Jesper Petersen <info@ordklar.dk>
2021-07-29 02:07:42 +0200

    Update Extensions.cs

--- BookSimpleAPI/Helper/Extensions/Extensions.cs
+++ BookSimpleAPI/Helper/Extensions/Extensions.cs
@@ -33,7 +33,7 @@ namespace BookSimpleAPI.Helper.Extensions
                 }
                 else
                 {
-                    return str;
+                    return null;
                 }
             }
 
